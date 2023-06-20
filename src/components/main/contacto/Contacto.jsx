3c3d7e8e539170356220c9184ca5521d4831f573
clasalde <calasalde@gmail.com>
2023-06-20 19:30:43 -0300

    Segunda Entrega 3.0

--- src/components/main/contacto/Contacto.jsx
+++ src/components/main/contacto/Contacto.jsx
@@ -3,6 +3,8 @@ const Contacto = () => {
     return (
         <div className="productos container d-flex flex-wrap justify-content-center">
             <h2 className="text-center my-5 py-3">CONTACTO</h2>
+            <hr />
+            <h4>Página en construcción</h4>
 
         </div>
     )
